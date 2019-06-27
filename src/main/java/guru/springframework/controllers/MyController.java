53697097a6421ac4aa1a4260587dfb236d6b0f50
josephcuellar <josephcuellar2662@gmail.com>
2019-06-26 20:50:33 -0600

    Added autowiring

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,4 +1,12 @@
 package guru.springframework.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+
+    public String hello() {
+        System.out.println("Hello");
+        return "hello";
+    }
 }
